f4b0abc88fbbf10b57216066208a8094b082a813
SAAJEVES <eveshofase@gmail.com>
2023-01-02 01:01:34 +0100

    fourth commit

--- src/Utils/formula.js
+++ src/Utils/formula.js
@@ -3,16 +3,16 @@ export const newPopulation = (value) => {
   const valueLen = valueStr.length;
   switch (valueLen) {
     case 4:
-      `${valueStr.slice(0, 1)},${valueStr.slice(1)}`;
+      return `${valueStr.slice(0, 1)},${valueStr.slice(1)}`;
       break;
     case 5:
-      `${valueStr.slice(0, 2)},${valueStr.slice(2)}`;
+      return `${valueStr.slice(0, 2)},${valueStr.slice(2)}`;
       break;
     case 6:
-      `${valueStr.slice(0, 3)},${valueStr.slice(3)}`;
+      return `${valueStr.slice(0, 3)},${valueStr.slice(3)}`;
       break;
     case 7:
-      `${valueStr.slice(0, 1)},${valueStr.slice(1, 4)},${valueStr.slice(4)}`;
+      return `${valueStr.slice(0, 1)},${valueStr.slice(1, 4)},${valueStr.slice(4)}`;
       break;
     case 8:
       return`${valueStr.slice(0, 2)},${valueStr.slice(2, 5)},${valueStr.slice(5)}`;
@@ -21,10 +21,10 @@ export const newPopulation = (value) => {
       return`${valueStr.slice(0, 3)},${valueStr.slice(3, 6)},${valueStr.slice(6)}`;
       break;
     case 10:
-      `${valueStr.slice(0, 1)},${valueStr.slice(1, 4)},${valueStr.slice(4, 7)},${valueStr.slice(7)}`;
+      return `${valueStr.slice(0, 1)},${valueStr.slice(1, 4)},${valueStr.slice(4, 7)},${valueStr.slice(7)}`;
       break;
     default:
-      value
+      return value
       break;
   }
 }
